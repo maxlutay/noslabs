3165191bbe8b8e9c957d0b3a3da50d1688fb7ad8
Maxim Lutay <maksym.lutai@nure.ua>
2019-01-22 17:09:52 +0200

    Add files via upload

--- lb3/3/v2.c
+++ lb3/3/v2.c
@@ -30,31 +30,28 @@ int main(){
     printf("Enter elements:");
     fgets(str_data,256,stdin);
 
-    char* tmp;
     int i = 0;
-    p.arr = (double*)malloc(p.size*sizeof(double));
-    tmp = strtok(str_data," ");
+    p.arr = (double*)malloc(p.size*sizeof(*p.arr));
+    char* tmp = strtok(str_data," ");
     p.arr[i++] = atof(tmp);
     
     while(NULL != (tmp=strtok(NULL," ")) ){  p.arr[i++] = atof(tmp);  }
 
     p.size = i;
-    //p.arr = realloc(p.arr, p.size * sizeof(int));
+    p.arr = realloc(p.arr, p.size * sizeof(*p.arr));
     
-
     printf("Entered %d numbers : ",i);
     while(i > 0){ printf("%4.2f ", p.arr[p.size - i--]); };
-
     printf("\n");
     
 
 
-    sem_init(&sem,0,0);
+    sem_init(&sem,0,0);// mode, value
     pthread_mutex_lock(&mut);
     
     pthread_t th_work, th_SumElement;
     int iret_w, iret_se;
-    iret_w = pthread_create(&th_work, NULL,work,(void*)&p);//pass ptr to array structure
+    iret_w = pthread_create(&th_work, NULL,work,(void*)&p);//pass ptr on structure
     iret_se = pthread_create(&th_SumElement, NULL, sumelement, (void*)&p);//-||-
     
     for(int i = 0; i < p.size; i++){ 
@@ -100,12 +97,12 @@ void* work(void* _args){
                 p.arr[li + countfound] = tmp;
                 //endswap
                 
-                if(countfound==1) sem_post(&sem);
-                sem_post(&sem);//notify has clone
+                if(countfound==1){
+                    sem_post(&sem);//notify original
+                    sleep(sleeptime);
+                }; 
+                sem_post(&sem);//notify clone
 
-
-                               
- 
                 sleep(sleeptime);
             };
         };
@@ -117,13 +114,13 @@ void* work(void* _args){
             double tmp = p.arr[li];
             p.arr[li] = p.arr[ri];
             p.arr[ri]=tmp;
-    
-            //sem_post(&sem1);//notify no clones
-            sleep(sleeptime);
+            //endswap
         };
-
     };
-    for(int i = ri; i <= p.size; i++) sem_post(&sem);
+    for(int i = ri; ri <= p.size; ri++) {
+        sem_post(&sem);//notify rest unique
+        sleep(sleeptime);
+    };
 }
 
 
